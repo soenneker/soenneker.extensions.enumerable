6edad78ef63c424e551b3067dfd7b99d03a206c3
Jake Soenneker <jake@soenneker.com>
2023-02-16 11:18:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.Enumerable.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Enumerable/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.enumerable/main.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.enumerable/actions/workflows/main.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.Enumerable.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Enumerable/)
+
+# Soenneker.Extensions.Enumerable
+### A collection of helpful enumerable extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.Enumerable
+```
+
+## Usage
+
+### `IEnumerable` should have `IsNullOrEmpty()` too
+
+```csharp
+var populatedList = new List<string>{"foo", "bar", "foo"};
+
+populatedList.IsNullOrEmpty() // false
+
+populatedList.Populated() // true
+populatedList.None() // false
+```
+
+## One call checking for null and contains any elements
+
+```csharp
+List<string>? nullList = null;
+
+nullList.IsNullOrEmpty() // true
+nullList.Populated() // false
+```
+
+### Duplicate handling
+
+```csharp
+var containsDuplicates = populatedList.ContainsDuplicates(); // true
+
+var deduped = populatedList.RemoveDuplicates(); // {"foo", "bar"}
+```
+
+### Recursive flattening
+
+```csharp
+public class Node 
+{
+    public string Name {get; set;}
+    public List<Node> Children {get; set;}
+}
+
+void Example()
+{
+    var node = new Node(){ Name = "Node1" };
+    node.Children = new List()
+    {
+        new Node() 
+        {
+            Name = "Node2"
+        }
+    }
+
+    List<Node>? children = node.Children.ToFlattenedFromRecursive(c => c.Children);
+
+    // Results in flattened List:
+    // { Node1, Node2 }
+}
+```
---
+++ src/EnumerableExtension.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Soenneker.Extensions.Enumerable;
+
+/// <summary>
+/// A collection of helpful enumerable extension methods
+/// </summary>
+public static class EnumerableExtension
+{
+    /// <summary>
+    /// Determines whether the collection is null or contains no elements.
+    /// </summary>
+    /// <typeparam name="T">The IEnumerable type.</typeparam>
+    /// <param name="enumerable">The enumerable, which may be null or empty.</param>
+    /// <returns>
+    ///     <c>true</c> if the IEnumerable is null or empty; otherwise, <c>false</c>.
+    /// </returns>
+    [Pure]
+    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable)
+    {
+        if (enumerable == null)
+            return true;
+
+        return None(enumerable);
+    }
+
+    /// <summary>
+    /// Shorthand for <see cref="IsNullOrEmpty{T}"/> == false.
+    /// </summary>
+    /// <remarks>Makes calls like <code>if (!list.IsNullOrEmpty())</code> easier to read</remarks>
+    [Pure]
+    public static bool Populated<T>([NotNullWhen(true)] this IEnumerable<T>? enumerable)
+    {
+        return !IsNullOrEmpty(enumerable);
+    }
+
+    /// <summary>
+    /// Determines whether the collection is empty <para></para>
+    /// Assumes collection is non-null!
+    /// </summary>
+    /// <exception cref="ArgumentNullException"></exception>
+    [Pure]
+    public static bool None<T>(this IEnumerable<T> enumerable)
+    {
+        if (enumerable == null)
+            throw new ArgumentNullException(nameof(enumerable));
+
+        /* If this is a list, use the Count property for efficiency. 
+        * The Count property is O(1) while IEnumerable.Count() is O(N). */
+
+        if (enumerable is ICollection<T> collection)
+            return collection.Count == 0;
+
+        if (enumerable is IReadOnlyCollection<T> readonlyCollection) // Need this because IReadOnlyCollection does not inherit from ICollection
+            return readonlyCollection.Count == 0;
+
+        return !enumerable.Any();
+    }
+
+    [Pure]
+    public static IEnumerable<T> RemoveDuplicates<T>(this IEnumerable<T> enumerable)
+    {
+        // Deduplication within ctor
+        var hashSet = new HashSet<T>(enumerable);
+
+        return hashSet;
+    }
+
+    /// <summary>
+    /// Iterates through the enumerable, adding each item to a HashSet. If it ever fails to add it early exits and returns true.
+    /// </summary>
+    [Pure]
+    public static bool ContainsDuplicates<T>(this IEnumerable<T> enumerable)
+    {
+        var hashSet = new HashSet<T>();
+        
+        foreach (T item in enumerable)
+        {
+            bool result = hashSet.Add(item);
+
+            if (!result)
+                return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Flattens and returns a collection of recursive children. Preserves underlying references even though it's a new list.
+    /// </summary>
+    /// <returns>Null if the source collection is is null. Otherwise returns an empty list if there are no children.</returns>
+    [Pure]
+    public static List<T>? ToFlattenedFromRecursive<T>(this IEnumerable<T>? enumerable, Expression<Func<T, IEnumerable<T>?>> childCollection)
+    {
+        if (enumerable == null)
+            return null;
+
+        var resultList = new List<T>();
+        var currentItems = new Queue<(int Index, T Item, int Depth)>(enumerable.Select(i => (0, i, 0)));
+        var depthItemCounter = 0;
+        var previousItemDepth = 0;
+        var childProperty = (PropertyInfo)((MemberExpression)childCollection.Body).Member;
+        while (currentItems.Count > 0)
+        {
+            (int Index, T Item, int Depth) currentItem = currentItems.Dequeue();
+            // Reset counter for number of items at this depth when the depth changes.
+            if (currentItem.Depth != previousItemDepth) depthItemCounter = 0;
+            int resultIndex = currentItem.Index + depthItemCounter++;
+            resultList.Insert(resultIndex, currentItem.Item);
+
+            IEnumerable<T> childItems = childProperty.GetValue(currentItem.Item) as IEnumerable<T> ?? System.Linq.Enumerable.Empty<T>();
+            foreach (T childItem in childItems)
+            {
+                currentItems.Enqueue((resultIndex + 1, childItem, currentItem.Depth + 1));
+            }
+            previousItemDepth = currentItem.Depth;
+        }
+
+        return resultList;
+    }
+}
